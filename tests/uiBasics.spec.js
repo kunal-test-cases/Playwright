82bfd805e0c23c3d086052b31493aa30efe175c4
Kunal Pardeshi <kunalpardeshi77@gmail.com>
2023-01-11 10:55:28 +0530

    playwright.config.js

--- tests/uiBasics.spec.js
+++ tests/uiBasics.spec.js
@@ -7,5 +7,10 @@ test("browser context playwright test", async ({ browser }) => {
 });
 
 test("page playwright test", async ({ page }) => {
-  await page.goto("https://rahulshettyacademy.com/locatorspractice/");
+  await page.goto("https://google.com/");
+});
+
+//Playwright will run below test only and skip all the remaining tests in this file.
+test.only("page playwright test only", async ({ page }) => {
+  await page.goto("https://google.com/");
 });
