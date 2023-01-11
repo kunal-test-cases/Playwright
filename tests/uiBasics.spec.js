ee27fbbebb10856f1f2cb2be5c9ae51998256ffb
Kunal Pardeshi <kunalpardeshi77@gmail.com>
2023-01-11 22:48:31 +0530

    locator - type and click on elements

--- tests/uiBasics.spec.js
+++ tests/uiBasics.spec.js
@@ -11,9 +11,20 @@ test("page playwright test", async ({ page }) => {
 });
 
 //Playwright will run below test only and skip all the remaining tests in this file.
-test.only("page playwright test only", async ({ page }) => {
+test("page playwright test only", async ({ page }) => {
   await page.goto("https://google.com/");
 
   //assert page title
   await expect(page).toHaveTitle("Google");
+});
+
+test.only("type into input elements using page locators", async ({
+  browser,
+}) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto("https://rahulshettyacademy.com/locatorspractice/");
+  await page.locator("input#inputUsername").type("kunal");
+  await page.locator("[name='inputPassword']").type("12345");
+  await page.locator("button.signInBtn").click();
 });
