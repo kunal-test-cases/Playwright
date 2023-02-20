40faf2ec9a71113d6cbabc0ef3094b7ccd209c93
Kunal Pardeshi <kunalpardeshi77@gmail.com>
2023-02-20 23:06:24 +0530

    locator - handling ui elements

---
+++ tests/uiControls.spec.js
@@ -0,0 +1,36 @@
+const { test, expect } = require("@playwright/test");
+
+test("Handling UI elements", async ({ page }) => {
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise");
+
+  const userName = page.locator("input#username");
+  const password = page.locator("input#password");
+  const signIn = page.locator("input#signInBtn");
+
+  //select dropdown
+  const dropdown = page.locator("select.form-control");
+  await dropdown.selectOption("consult");
+
+  //select radio
+  //await page.locator("radiotextsty").last().click();
+  const radio = page.locator("input[type='radio'][value='user']");
+  await radio.click();
+
+  await page.locator("#okayBtn").click();
+
+  //assert radio btn is checked.
+  await expect(radio).toBeChecked();
+  console.log(await radio.isChecked());
+
+  //select checkbox
+  const checkbox = page.locator("#terms");
+  await checkbox.click();
+
+  //assert checkbox is checked.
+  await expect(checkbox).toBeChecked();
+  console.log(await checkbox.isChecked());
+  await checkbox.uncheck();
+  await expect(await checkbox.isChecked()).toBeFalsy();
+
+  await page.pause();
+});
