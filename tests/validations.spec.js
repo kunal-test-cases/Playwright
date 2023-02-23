55d87da58ef3de375be739dccf62b58e2e68f3c8
Kunal Pardeshi <kunalpardeshi77@gmail.com>
2023-02-23 10:56:15 +0530

    assert show/hide element in playwright

--- tests/validations.spec.js
+++ tests/validations.spec.js
@@ -10,3 +10,15 @@ test("browser forward and backward", async ({ page }) => {
   //browser forward button
   await page.goForward();
 });
+
+test("input element show/hide", async ({ page }) => {
+  await page.goto("https://rahulshettyacademy.com/AutomationPractice");
+  expect(await page.locator("#displayed-text").isVisible()).toBeTruthy();
+  await page.locator("#hide-textbox").click();
+  expect(await page.locator("#displayed-text").isHidden()).toBeTruthy();
+
+  await page.locator("#show-textbox").click();
+  await expect(await page.locator("#displayed-text")).toBeVisible();
+  await page.locator("#hide-textbox").click();
+  await expect(await page.locator("#displayed-text")).toBeHidden();
+});
